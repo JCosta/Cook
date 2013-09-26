b65f65a343893d5c58e7519fd6336d1b2c24ecee
Hugo Maganinho <hmaganinho@gmail.com>
2013-09-26 14:17:53 +0100

    tarde 5ªF
    inicio trab tarde

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,7 +2,8 @@ class RecipesController <ApplicationController
 
  def index
  #	@recipes = Recipes.where(group: "peixe")
- 	@recipes = Recipe.all
+# 	@recipes = Recipe.all
+ 	@recipes = Recipe.limit(4).all
  end
 
  def show
